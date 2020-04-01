52e99f9f327ec59e50c4a404ae36f8fb12be321b
“YingZheng” <ikingdom778@gmail.com>
2020-04-01 23:35:29 +0900

    [新機能][仕訳画面] データベース環境構築
    Realmをインストール

---
+++ iOS/Accountant/v1.0/Program/src/Accountant/JournalEntry/Pickerkeyboard.swift
@@ -0,0 +1,21 @@
+//
+//  Pickerkeyboard.swift
+//  Accountant
+//
+//  Created by Hisashi Ishihara on 2020/04/01.
+//  Copyright © 2020 Hisashi Ishihara. All rights reserved.
+//
+
+import UIKit
+
+class Pickerkeyboard: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
