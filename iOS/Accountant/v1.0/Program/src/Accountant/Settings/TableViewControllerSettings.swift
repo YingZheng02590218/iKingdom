462f2f85e32877e1cc4c297b4cbd9208d2e9dfbe
“YingZheng” <ikingdom778@gmail.com>
2020-05-22 12:29:33 +0900

    [新機能][設定画面] 画面作成
    勘定科目設定はマスターデータをCSV形式で作成し、一気に読み込めるようにした。

--- iOS/Accountant/v1.0/Program/src/Accountant/Settings/TableViewControllerSettings.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/Settings/TableViewControllerSettings.swift
@@ -24,6 +24,7 @@ class TableViewControllerSettings: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
+//        return 2
         return 1
     }
 
@@ -34,14 +35,30 @@ class TableViewControllerSettings: UITableViewController {
 
     // セクションヘッダーのテキスト決める
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        return "勘定科目"
+        switch section {
+        case 0:
+//            return "帳簿"
+//        case 1:
+            return "勘定科目"
+        default:
+            return ""
+        }
     }
-    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch section {
+        case 0:
+//            return "利用する補助簿を設定することができます。"
+//        case 1:
+            return "利用する勘定科目を設定することができます。"
+        default:
+            return ""
+        }
+    }
     //セルを生成して返却するメソッド
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> TableViewCellSettings {
         //① UI部品を指定　TableViewCell
-        let cell = TableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_list_settings", for: indexPath) as! TableViewCellSettings
+        
         return cell
     }
     /*
