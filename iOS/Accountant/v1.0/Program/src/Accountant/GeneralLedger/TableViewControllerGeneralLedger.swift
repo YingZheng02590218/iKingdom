23ec5631cdddf92e3198493d7d2a090f2a4e50f1
“YingZheng” <ikingdom778@gmail.com>
2020-05-28 12:51:07 +0900

    [新機能][勘定画面] テーブル部分作成

---
+++ iOS/Accountant/v1.0/Program/src/Accountant/GeneralLedger/TableViewControllerGeneralLedger.swift
@@ -0,0 +1,143 @@
+//
+//  TableViewControllerGeneralRedger.swift
+//  Accountant
+//
+//  Created by Hisashi Ishihara on 2020/03/23.
+//  Copyright © 2020 Hisashi Ishihara. All rights reserved.
+//
+
+import UIKit
+
+class TableViewControllerGeneralLedger: UITableViewController {
+
+    @IBOutlet var TableView_generalLedger: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 5
+    }
+    // セクションヘッダーのテキスト決める
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "資産の部"
+        case 1:
+            return "負債の部"
+        case 2:
+            return "純資産の部"
+        case 3:
+            return "費用の部"
+        case 4:
+            return "収益の部"
+        default:
+            return ""
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // データベース
+        let databaseManagerSettings = DatabaseManagerSettingsCategory() //データベースマネジャー
+        // セクション毎に分けて表示する。indexPath が row と section を持っているので、sectionで切り分ける。ここがポイント
+        let objects = databaseManagerSettings.getSettings(section: section) // どのセクションに表示するセルかを判別するため引数で渡す
+        return objects.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // データベース
+        let databaseManagerSettings = DatabaseManagerSettingsCategory() //データベースマネジャー
+        // セクション毎に分けて表示する。indexPath が row と section を持っているので、sectionで切り分ける。ここがポイント
+        let objects = databaseManagerSettings.getSettings(section: indexPath.section) // どのセクションに表示するセルかを判別するため引数で渡す
+        //① UI部品を指定　TableViewCellCategory
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_list_generalLedger", for: indexPath)
+        // 勘定科目の名称をセルに表示する
+        cell.textLabel?.text = "\(objects[indexPath.row].category as String)"
+        cell.textLabel?.textAlignment = NSTextAlignment.center
+        
+        return cell
+    }
+    
+//    var account :String = "" // 勘定名
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        // 選択されたセルを取得
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_list_generalLedger", for: indexPath) as! TableViewCellGeneralLedger
+//        account = String(cell.textLabel!.text!) // セルに表示した勘定名を取得
+//        // セルの選択を解除
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        // 別の画面に遷移
+//        performSegue(withIdentifier: "identifier_generalLedger", sender: nil)
+//    }
+    // 画面遷移の準備　勘定科目画面
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 選択されたセルを取得
+        let indexPath: IndexPath = self.TableView_generalLedger.indexPathForSelectedRow! // ※ didSelectRowAtの代わりにこれを使う方がいい　タップされたセルの位置を取得
+        // データベース
+        let databaseManagerSettings = DatabaseManagerSettingsCategory() //データベースマネジャー
+        // セクション毎に分けて表示する。indexPath が row と section を持っているので、sectionで切り分ける。ここがポイント
+        let objects = databaseManagerSettings.getSettings(section: indexPath.section) // どのセクションに表示するセルかを判別するため引数で渡す
+//        let cell = self.TableView_generalLedger.dequeueReusableCell(withIdentifier: "cell_list_generalLedger", for: indexPath)
+        // segue.destinationの型はUIViewController
+        let viewControllerGenearlLedgerAccount = segue.destination as! ViewControllerGenearlLedgerAccount
+        // 遷移先のコントローラに値を渡す
+        viewControllerGenearlLedgerAccount.account = "\(objects[indexPath.row].category as String)" // セルに表示した勘定名を取得
+        // セルの選択を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/Accountant/v1.0/Program/src/Accountant/GeneralLedger/ViewControllerGenearlLedgerAccount.swift
@@ -0,0 +1,169 @@
+//
+//  ViewControllerGenearlLedgerAccount.swift
+//  Accountant
+//
+//  Created by Hisashi Ishihara on 2020/05/27.
+//  Copyright © 2020 Hisashi Ishihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerGenearlLedgerAccount: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var TableView_account: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        TableView_account.delegate = self
+        TableView_account.dataSource = self
+        // ヘッダー部分　勘定名を表示
+        label_list_heading.text = account
+        //3桁ごとにカンマ区切りするフォーマット
+        formatter.numberStyle = NumberFormatter.Style.decimal
+        formatter.groupingSeparator = ","
+        formatter.groupingSize = 3
+    }
+
+    // セクションの数を設定する
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 12     // セクションの数はreturn 12 で 12ヶ月分に設定します。
+    }
+    // セクションヘッダーの高さを決める
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 33 //セクションヘッダーの高さを33に設定　セルの高さより高くしてメリハリをつける セル(Row Hight 30)
+    }
+    // セクションヘッダーの色とか調整する
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = UIColor.gray
+        header.textLabel?.textAlignment = .left
+//        let attributedStr = NSMutableAttributedString(string: header.textLabel?.text)
+//        let crossAttr = [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue]
+//        header.textLabel?.text = attributedStr
+    }
+    // セクションヘッダーのテキスト決める
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var section_num = section + 4//4月スタートに補正する todo 設定の決算月によって変更する
+        //セクションヘッダーは1年分の12ヶ月にする
+        if section_num >= 13 {  //12ヶ月を超えた場合1月に戻す
+            section_num -= 12
+        }
+        var mon = "  月"
+        if section_num > 9 {
+            mon = "月"
+        }
+        let header_title = section_num.description + mon
+        return header_title
+    }
+    //セルの数を、モデル(仕訳)の数に指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // データベース
+        let dataBaseManagerAccount = DataBaseManagerAccount() //データベースマネジャー
+        let counts = dataBaseManagerAccount.getAccountCounts(section: section, account: account) // 何月のセクションに表示するセルかを引数で渡す
+//        print("月別のセル数:\(counts)")
+        return counts //月別の仕訳データ数
+    }
+    
+    var account :String = "" // 勘定名
+    @IBOutlet weak var label_list_heading: UILabel!
+    //セルを生成して返却するメソッド
+    var indexPathForAutoScroll: IndexPath = IndexPath(row: 0, section: 0)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // データベース
+        let dataBaseManagerAccount = DataBaseManagerAccount() //データベースマネジャー
+        // セクション毎に分けて表示する。indexPath が row と section を持っているので、sectionで切り分ける。ここがポイント
+        let objects = dataBaseManagerAccount.getAccount(section: indexPath.section, account: account) // 何月のセクションに表示するセルかを判別するため引数で渡す
+//        print("月別のセル:\(objects)")
+        //① UI部品を指定　TableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_list_generalLedger_account", for: indexPath) as! TableViewCellGeneralLedgerAccount
+        
+        //② todo 借方の場合は左寄せ、貸方の場合は右寄せ。小書きは左寄せ。
+        
+        let d = "\(objects[indexPath.row].date)" // 日付
+        // 月別のセクションのうち、日付が一番古いものに月欄に月を表示し、それ以降は空白とする。
+        if indexPath.row == 0 {
+            let dateMonth = d[d.index(d.startIndex, offsetBy: 5)..<d.index(d.startIndex, offsetBy: 6)] // 日付の6文字目にある月の十の位を抽出
+            if dateMonth == "0" { // 日の十の位が0の場合は表示しない
+                cell.label_list_date_month.text = "\(d[d.index(d.startIndex, offsetBy: 6)..<d.index(d.startIndex, offsetBy: 7)])" // 「月」
+            }else{
+                cell.label_list_date_month.text = "\(d[d.index(d.startIndex, offsetBy: 5)..<d.index(d.startIndex, offsetBy: 7)])" // 「月」
+            }
+        }else{
+            cell.label_list_date_month.text = "" // 注意：空白を代入しないと、変な値が入る。
+        }
+        let date = d[d.index(d.startIndex, offsetBy: 8)..<d.index(d.startIndex, offsetBy: 9)] // 日付の9文字目にある日の十の位を抽出
+        if date == "0" { // 日の十の位が0の場合は表示しない
+            cell.label_list_date_day.text = "\(objects[indexPath.row].date.suffix(1))" // 末尾1文字の「日」         //日付
+        }else{
+            cell.label_list_date_day.text = "\(objects[indexPath.row].date.suffix(2))" // 末尾2文字の「日」         //日付
+        }
+        cell.label_list_date_day.textAlignment = NSTextAlignment.right
+//        cell.label_list_summary_debit.text = " (\(objects[indexPath.row].debit_category))"     //借方勘定
+//        cell.label_list_summary_debit.textAlignment = NSTextAlignment.left
+//        cell.label_list_summary_credit.text = "(\(objects[indexPath.row].credit_category)) "   //貸方勘定
+//        cell.label_list_summary_credit.textAlignment = NSTextAlignment.right
+        if account == "\(objects[indexPath.row].debit_category)" {                                   //この勘定が借方の場合
+            cell.label_list_summary.text = "\(objects[indexPath.row].credit_category) "              //摘要　相手方勘定なので貸方
+            cell.label_list_summary.textAlignment = NSTextAlignment.right
+            cell.label_list_debit.text = "\(addComma(string: String(objects[indexPath.row].debit_amount))) "        //借方金額
+            cell.label_list_credit.text = ""                                                                        //貸方金額
+        }else if account == "\(objects[indexPath.row].credit_category)" {
+            cell.label_list_summary.text = "\(objects[indexPath.row].debit_category) "               //摘要　相手方勘定なので借方
+            cell.label_list_summary.textAlignment = NSTextAlignment.left
+            cell.label_list_debit.text = ""                                                                         //借方金額
+            cell.label_list_credit.text = "\(addComma(string: String(objects[indexPath.row].credit_amount))) "      //貸方金額
+        }
+
+        // ToDo 勘定科目の番号
+        cell.label_list_number.text = "1" // 丁数
+        cell.label_list_debitOrCredit.text = "1"        // 借又貸
+        cell.label_list_balance.text = "ToDo"        //差引残高
+        
+        return cell
+    }
+    //カンマ区切りに変換（表示用）
+    let formatter = NumberFormatter() // プロパティの設定はviewDidLoadで行う
+    func addComma(string :String) -> String{
+        if(string != "") { // ありえないでしょう
+            return formatter.string(from: NSNumber(value: Double(string)!))!
+        }else{
+            return ""
+        }
+    }
+//    // セルが画面に表示される直前に表示される
+//    var scroll = false   // flag 初回起動後かどうかを判定する (viewDidLoadでON, viewDidAppearでOFF)
+//    var scroll_adding = false   // flag 入力ボタン押下後かどうかを判定する (autoScrollでON, viewDidAppearでOFF)
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        var indexPath_local = IndexPath(row: 0, section: 0)
+//        if scroll || scroll_adding {     // 初回起動時の場合 入力ボタン押下時の場合
+//            for s in 0..<TableView_account.numberOfSections-1 {            //セクション数　ゼロスタート補正
+//                if TableView_account.numberOfRows(inSection: s) > 0 {
+//                    let r = TableView_account.numberOfRows(inSection: s)-1 //セル数　ゼロスタート補正
+//                    indexPath_local = IndexPath(row: r, section: s)
+//                    self.TableView_account.scrollToRow(at: indexPath_local, at: UITableView.ScrollPosition.top, animated: false) // topでないとタブバーの裏に隠れてしまう　animatedはありでもよい
+//                }
+//            }
+//        }
+//        if scroll_adding {     // 入力ボタン押下時の場合
+//            // 新規追加した仕訳データのセルを作成するために、最後の行までスクロールする　→ セルを作成時に位置を覚える
+//            if indexPath == indexPath_local { // 最後のセルまで表示しされたかどうか
+//                self.TableView_account.scrollToRow(at: indexPathForAutoScroll, at: UITableView.ScrollPosition.bottom, animated: false) // 追加した仕訳データの行を画面の下方に表示する
+//                // 入力ボタン押下時の表示位置 OFF
+//                scroll_adding = false
+//            }
+//        }
+//    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
