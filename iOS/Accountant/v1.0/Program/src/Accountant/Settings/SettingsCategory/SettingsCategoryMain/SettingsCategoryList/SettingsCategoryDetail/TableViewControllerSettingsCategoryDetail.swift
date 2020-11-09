2445b7e97189d4f15c1afc3b971d02c5e22365eb
“YingZheng” <ikingdom778@gmail.com>
2020-11-10 04:59:35 +0900

    [新機能][勘定科目詳細画面] マネタイズ対応 [その他][複数画面] レイアウト調整

--- iOS/Accountant/v1.0/Program/src/Accountant/Settings/SettingsCategory/SettingsCategoryMain/SettingsCategoryList/SettingsCategoryDetail/TableViewControllerSettingsCategoryDetail.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/Settings/SettingsCategory/SettingsCategoryMain/SettingsCategoryList/SettingsCategoryDetail/TableViewControllerSettingsCategoryDetail.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import GoogleMobileAds // マネタイズ対応
 
 // 勘定科目　詳細画面
 class TableViewControllerSettingsCategoryDetail: UITableViewController, UITextFieldDelegate {
 
+    // マネタイズ対応
+    // 広告ユニットID
+    let AdMobID = "ca-app-pub-7616440336243237/8565070944"
+    // テスト用広告ユニットID
+    let TEST_ID = "ca-app-pub-3940256099942544/2934735716"
+    // true:テスト
+    let AdMobTest:Bool = false
+    @IBOutlet var gADBannerView: GADBannerView!
+    
     var big = ""
     var mid = ""
     var small = ""
@@ -38,7 +48,52 @@ class TableViewControllerSettingsCategoryDetail: UITableViewController, UITextFi
     override func viewWillAppear(_ animated: Bool) {
         // 表示科目を変更後に勘定科目詳細画面を更新する
         tableView.reloadData()
+        
+        // マネタイズ対応　完了　注意：viewDidLoad()ではなく、viewWillAppear()に実装すること
+        print("Google Mobile Ads SDK version: \(GADRequest.sdkVersion())")
+        // GADBannerView を作成する
+        gADBannerView = GADBannerView(adSize:kGADAdSizeLargeBanner)
+        // iPhone X のポートレート決め打ちです　→ 仕訳帳のタブバーの上にバナー広告が表示されるように調整した。
+//        print(self.view.frame.size.height)
+//        print(gADBannerView.frame.height)
+//        gADBannerView.frame.origin = CGPoint(x: 0, y: self.view.frame.size.height - gADBannerView.frame.height + tableView.contentOffset.y) // スクロール時の、広告の位置を固定する
+//        gADBannerView.frame.size = CGSize(width: self.view.frame.width, height: gADBannerView.frame.height)
+        // GADBannerView プロパティを設定する
+        if AdMobTest {
+            gADBannerView.adUnitID = TEST_ID
+        }
+        else{
+            gADBannerView.adUnitID = AdMobID
+        }
+        gADBannerView.rootViewController = self
+        // 広告を読み込む
+        gADBannerView.load(GADRequest())
+        print(tableView.visibleCells[tableView.visibleCells.count-1].frame.height)
+        // GADBannerView を作成する
+//        addBannerViewToView(gADBannerView, constant: 0)
+        addBannerViewToView(gADBannerView, constant: tableView.visibleCells[tableView.visibleCells.count-1].frame.height * -1)
     }
+    
+    func addBannerViewToView(_ bannerView: GADBannerView, constant: CGFloat) {
+      bannerView.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(bannerView)
+      view.addConstraints(
+        [NSLayoutConstraint(item: bannerView,
+                            attribute: .bottom,
+                            relatedBy: .equal,
+                            toItem: bottomLayoutGuide,
+                            attribute: .top,
+                            multiplier: 1,
+                            constant: constant),
+         NSLayoutConstraint(item: bannerView,
+                            attribute: .centerX,
+                            relatedBy: .equal,
+                            toItem: view,
+                            attribute: .centerX,
+                            multiplier: 1,
+                            constant: 0)
+        ])
+     }
     
     // MARK: - Table view data source
 
