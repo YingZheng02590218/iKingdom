3d035cbbd8513518d5d3c18ad96f40c0ecc2c485
“YingZheng” <ikingdom778@gmail.com>
2020-12-06 16:00:37 +0900

    [不具合修正][仕訳画面] 小書き　入力チェック　カンマを排他 小書き　backspaceキー有効化
    [不具合修正][帳簿情報画面]
    事業者名　入力チェック　カンマを排他
    事業者名　入力チェック　リターンキー押下でキーボードを閉じる
    事業者名　backspaceキー有効化
    
    [その他][勘定科目一覧画面]
    スワイプ削除　無効化
    
    [不具合修正][勘定科目詳細画面]
    勘定科目名　入力チェック　カンマを排他
    勘定科目名　入力チェック　リターンキー押下でキーボードを閉じる
    勘定科目名　backspaceキー有効化
    セルのアクセサリーを追加　表示科目は　disclosureIndicator

--- iOS/Accountant/v1.0/Program/src/Accountant/Settings/SettingsCategory/SettingsCategoryMain/SettingsCategoryList/TableViewControllerCategoryList.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/Settings/SettingsCategory/SettingsCategoryMain/SettingsCategoryList/TableViewControllerCategoryList.swift
@@ -283,4 +283,34 @@ class TableViewControllerCategoryList: UITableViewController {
             tableView.deselectRow(at: indexPath, animated: true)
         }
     }
+//    // セル選択不可
+//    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+//        // 編集モードの場合　は押下できないのでこの処理は通らない
+//        if tableView.isEditing {
+//            return indexPath
+//        }else {
+//            // 選択不可にしたい場合は"nil"を返す
+//            return nil
+//        }
+//    }
+     // 削除機能 セルを左へスワイプ
+        override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            // 編集モードの場合
+            if tableView.isEditing {
+                // スタイルには、normal と　destructive がある
+                let action = UIContextualAction(style: .destructive, title: "削除") { (action, view, completionHandler) in
+                    // なんか処理
+                    // 確認のポップアップを表示したい
+                    self.showPopover(indexPath: indexPath)
+                    completionHandler(true) // 処理成功時はtrue/失敗時はfalseを設定する
+                }
+                action.image = UIImage(systemName: "trash.fill") // 画像設定（タイトルは非表示になる）
+                let configuration = UISwipeActionsConfiguration(actions: [action])
+                return configuration
+            }else { // 編集モードではない状態でセルをスワイプした場合
+                let configuration = UISwipeActionsConfiguration(actions: [])
+                configuration.performsFirstActionWithFullSwipe = false
+                return configuration
+            }
+        }
 }
