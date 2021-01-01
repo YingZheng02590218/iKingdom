f8675f119dcbd88d6929eed1c1fffcf334f7e710
“YingZheng” <ikingdom778@gmail.com>
2021-01-01 22:10:28 +0900

    [不具合修正][精算表画面] 修正記入の合計欄を修正
    修正記入の合計欄に残高が入っていたので、合計に変更した。
    
    [その他][貸借対照表画面] レイアウト調整
    [その他][損益計算書画面] レイアウト調整
    [その他][精算表画面] レイアウト調整
    [その他][試算表画面] レイアウト調整
    
    ナビゲーションバーとタブバーの色がグレーになっていた。白くするために、Under Top Bars, Under Bottom Bars にチェックを入れた。そして、ナビゲーションバーとタブバーの下に潜り込まないように、Insetsで調整した。

--- iOS/Accountant/v1.0/Program/src/Accountant/FinancialStatement/PL/TableViewControllerPL.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/FinancialStatement/PL/TableViewControllerPL.swift
@@ -523,7 +523,9 @@ class TableViewControllerPL: UITableViewController, UIPrintInteractionController
             }else{
                 if self.navigationController?.navigationBar.bounds.height != nil { //ナビゲーションバーが見えない状態で画面遷移をするとnilとなる　2020/07/12 22:45
                     // インセットを設定する　ステータスバーとナビゲーションバーより下からテーブルビューを配置するため
-                    scrollView.contentInset = UIEdgeInsets(top: +self.navigationController!.navigationBar.bounds.height+UIApplication.shared.statusBarFrame.height, left: 0, bottom: 0, right: 0)
+//                    scrollView.contentInset = UIEdgeInsets(top: +self.navigationController!.navigationBar.bounds.height+UIApplication.shared.statusBarFrame.height, left: 0, bottom: 0, right: 0)
+                    // インセットを設定する　ステータスバーとナビゲーションバーより下からテーブルビューを配置するため
+                    scrollView.contentInset = UIEdgeInsets(top: +(UIApplication.shared.statusBarFrame.height+self.navigationController!.navigationBar.bounds.height), left: 0, bottom: (self.tabBarController?.tabBar.frame.size.height)!, right: 0)
                 }
             }
         }
