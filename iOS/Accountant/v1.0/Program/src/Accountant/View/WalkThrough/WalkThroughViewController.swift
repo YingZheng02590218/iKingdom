bde07c9b24ab9e6783d3bb6c572e7e6902174182
“YingZheng” <ikingdom778@gmail.com>
2022-05-20 18:30:24 +0900

    [新機能][ウォークスルー画面] 文言修正

--- iOS/Accountant/v1.0/Program/src/Accountant/View/WalkThrough/WalkThroughViewController.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/View/WalkThrough/WalkThroughViewController.swift
@@ -51,7 +51,7 @@ class WalkThroughViewController: UIViewController {
         page2.titleColor = UIColor.TextColor
         page2.titleFont = UIFont(name: "Helvetica-Bold", size: 40)
         page2.titlePositionY = self.view.bounds.size.height * 0.8
-        page2.desc = "紙の帳簿と同じデザインで\n仕訳帳と総勘定元帳を確認\n\n仕訳帳で、仕訳を編集や削除ができる"
+        page2.desc = "紙の帳簿と同じデザインで\n仕訳帳と総勘定元帳を確認\n\n仕訳帳で、仕訳の編集や削除ができる"
         page2.descColor = UIColor.lightGray
         page2.descFont = UIFont(name: "HiraMaruProN-W4", size: 20)
         page2.descPositionY = self.view.bounds.size.height * 0.8
