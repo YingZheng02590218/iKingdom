890b1d63bfabad91f4ab5af01ea97b08dd9314e1
“YingZheng” <ikingdom778@gmail.com>
2022-07-07 21:31:10 +0900

    [その他][仕訳帳画面] オートスクロール機能を改善
    仕訳入力後、テーブルビューを一度最下行までスクロールする
    入力した仕訳がなかった場合、最上行までスクロールする
    入力した仕訳の行をハイライトする

--- iOS/Accountant/v1.0/Program/src/Accountant/View/Journals/Journals/JournalsViewController.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/View/Journals/Journals/JournalsViewController.swift
@@ -57,6 +57,7 @@ class JournalsViewController: UIViewController, UIGestureRecognizerDelegate {
     var Number: Int? // 編集した仕訳の連番
     var TappedIndexPathSection: Int? // 通常仕訳か決算整理仕訳か
     private var indexPathForAutoScroll: IndexPath?
+    var indexPath_local = IndexPath(row: 0, section: 0) // 初期表示オートスクロール
     // セルが画面に表示される直前に表示される ※セルが0個の場合は呼び出されない
     var scroll = false   // flag 初回起動後かどうかを判定する (viewDidLoadでON, viewDidAppearでOFF)
     var scroll_adding = false   // flag 入力ボタン押下後かどうかを判定する (autoScrollでON, viewDidAppearでOFF)
@@ -497,7 +498,7 @@ extension JournalsViewController: UITableViewDelegate, UITableViewDataSource {
         }
         else {
             // 空白行
-            return 7 // 空白行を表示するため+7行を追加
+            return 5 // 空白行を表示するため+5行を追加
         }
     }
     //セルを生成して返却するメソッド
@@ -728,23 +729,40 @@ extension JournalsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        var indexPath_local = IndexPath(row: 0, section: 0)
-        if scroll || scroll_adding {     // 初回起動時の場合 入力ボタン押下時の場合
-            for s in 0..<tableView.numberOfSections {            //セクション数　ゼロスタート補正は不要
-                if tableView.numberOfRows(inSection: s) > 0 {
-                    let r = tableView.numberOfRows(inSection: s)-1 //セル数　ゼロスタート補正
+        // 初回起動時の場合
+        if scroll {
+            // セクション数　ゼロスタート補正は不要
+            for s in 0..<tableView.numberOfSections {
+                for r in 0..<tableView.numberOfRows(inSection: s) {
                     indexPath_local = IndexPath(row: r, section: s)
-                    self.tableView.scrollToRow(at: indexPath_local, at: UITableView.ScrollPosition.top, animated: true) // topでないとタブバーの裏に隠れてしまう　animatedはありでもよい
+                    self.tableView.scrollToRow(at: indexPath_local, at: UITableView.ScrollPosition.top, animated: false) // topでないとタブバーの裏に隠れてしまう　animatedはありでもよい
                 }
             }
+            // 最後のセルまで表示しされたかどうか
+            if indexPath == indexPath_local {
+                // 初期表示位置 OFF
+                scroll = false
+            }
         }
-        if scroll_adding {     // 入力ボタン押下時の場合
-            // 新規追加した仕訳データのセルを作成するために、最後の行までスクロールする　→ セルを作成時に位置を覚える
-            if indexPath == indexPath_local { // 最後のセルまで表示しされたかどうか
-                if let indexPathForAutoScroll = indexPathForAutoScroll {
-                    self.tableView.scrollToRow(at: indexPathForAutoScroll, at: UITableView.ScrollPosition.middle, animated: true) // 追加した仕訳データの行を画面の下方に表示する
+        // 入力ボタン押下時の場合
+        if scroll_adding {
+            // メソッドの引数 indexPath の変数 row には、セルのインデックス番号が設定されています。インデックス指定に利用する。
+            if Number == presenter.objects(forRow: indexPath.row).number { // 自動スクロール　入力ボタン押下時の戻り値と　仕訳番号が一致した場合
+                cell.setHighlighted(true, animated: true)
+                indexPathForAutoScroll = indexPath
+            }
+            // 最後のセルまで表示しされたかどうか
+            if indexPath == indexPath_local {
+                // 新規追加した仕訳データのセルを作成するために、最後の行までスクロールする　→ セルを作成時に位置を覚える
+                if let indexPathForAutoScroll = self.indexPathForAutoScroll {
+                    self.tableView.scrollToRow(at: indexPathForAutoScroll, at: UITableView.ScrollPosition.top, animated: true) // 追加した仕訳データの行を画面の下方に表示する
                     // 入力ボタン押下時の表示位置 OFF
-                    scroll_adding = false
+                    self.scroll_adding = false
+                    self.indexPathForAutoScroll = nil
+                }
+                else {
+                    // 上へスクロールする
+                    scrollToTop()
                 }
             }
         }
@@ -784,6 +802,34 @@ extension JournalsViewController: UITableViewDelegate, UITableViewDataSource {
                     if primaryKeysAdjusting[i] == presenter.objectsss(forRow:indexPath.row).number {
                         cell.setHighlighted(true, animated: true)
                     }
+                }
+            }
+        }
+    }
+    // 下へスクロールする
+    func scrollToBottom() {
+        // セクション数　ゼロスタート補正は不要
+        for s in 0..<tableView.numberOfSections {
+            for r in 0..<tableView.numberOfRows(inSection: s) {
+                indexPath_local = IndexPath(row: r, section: s)
+                self.tableView.scrollToRow(at: indexPath_local, at: UITableView.ScrollPosition.top, animated: true)
+                // topでないとタブバーの裏に隠れてしまう　animatedはありでもよい
+                if let _ = self.indexPathForAutoScroll {
+                    break
+                }
+            }
+        }
+    }
+    // 上へスクロールする
+    func scrollToTop() {
+        // セクション数　ゼロスタート補正は不要
+        for s in (0..<tableView.numberOfSections).reversed() {
+            for r in (0..<tableView.numberOfRows(inSection: s)).reversed() {
+                indexPath_local = IndexPath(row: r, section: s)
+                self.tableView.scrollToRow(at: indexPath_local, at: UITableView.ScrollPosition.bottom, animated: true)
+                // topでないとタブバーの裏に隠れてしまう　animatedはありでもよい
+                if let _ = self.indexPathForAutoScroll {
+                    break
                 }
             }
         }
@@ -1008,8 +1054,6 @@ extension JournalsViewController: JournalsPresenterOutput {
             // マネタイズ対応 bringSubViewToFrontメソッドを使い、広告を最前面に表示します。
             view.bringSubviewToFront(gADBannerView)
         }
-        // 初期表示位置 OFF
-        scroll = false
         let indexPath = tableView.indexPathsForVisibleRows // テーブル上で見えているセルを取得する
         print("tableView.indexPathsForVisibleRows: \(String(describing: indexPath))")
         // テーブルをスクロールさせる。scrollViewDidScrollメソッドを呼び出して、インセットの設定を行うため。
@@ -1036,7 +1080,7 @@ extension JournalsViewController: JournalsPresenterOutput {
     func autoScroll(number: Int, tappedIndexPathSection: Int) {
         // TabBarControllerから遷移してきした時のみ、テーブルビューの更新と初期表示位置を指定
         scroll_adding = true
-        
+        // 仕訳帳から仕訳入力
         Number = number
         TappedIndexPathSection = tappedIndexPathSection // 編集で選択されたセルのセクション
         // 前回の、まとめて編集後のセルのハイライトを戻す
@@ -1047,6 +1091,8 @@ extension JournalsViewController: JournalsPresenterOutput {
         self.tableView.reloadData()
         // ボタンを更新
         setButtons()
+        // 下へスクロールする
+        scrollToBottom()
     }
 }
 
