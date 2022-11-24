449e94777afa0446156380a75adad5707a8dc645
“YingZheng” <ikingdom778@gmail.com>
2022-11-25 02:13:04 +0900

    [新機能][勘定画面] 印刷機能　HTMLファイルをレイアウト調整

--- iOS/Accountant/v1.0/Program/src/Accountant/Functions/PDF/PDFMakerAccount.swift
+++ iOS/Accountant/v1.0/Program/src/Accountant/Functions/PDF/PDFMakerAccount.swift
@@ -16,7 +16,7 @@ class PDFMakerAccount {
     var PDFpath: [URL]?
 
     let hTMLhelper = HTMLhelperAccount()
-    let paperSize = CGSize(width: 210 / 25.4 * 72, height: 297 / 25.4 * 72) // A4 210×297mm
+    let paperSize = CGSize(width: 170 / 25.4 * 72, height: 257 / 25.4 * 72) // 調整した　A4 210×297mm
     // 勘定名
     var account: String = ""
     var fiscalYear = 0
@@ -194,6 +194,8 @@ class PDFMakerAccount {
         htmlString.append(footerString)
         //HTML -> PDF
         let pdfData = getPDF(fromHTML: htmlString)
+
+        print(htmlString)
         //PDFデータを一時ディレクトリに保存する
         if let fileName = saveToTempDirectory(data: pdfData) {
             //PDFファイルを表示する
@@ -212,6 +214,7 @@ class PDFMakerAccount {
         renderer.setValue(paperFrame, forKey: "printableRect")
         
         let formatter = UIMarkupTextPrintFormatter(markupText: fromHTML)
+        formatter.perPageContentInsets = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
         renderer.addPrintFormatter(formatter, startingAtPageAt: 0)
         
         let pdfData = NSMutableData()
