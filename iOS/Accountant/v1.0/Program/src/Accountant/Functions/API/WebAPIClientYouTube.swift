b5dbbf3fe470751b78ecfac42da826a30fa4a15b
“YingZheng” <ikingdom778@gmail.com>
2023-01-24 12:31:14 +0900

    [新機能][YouTube画面] サンプルアプリから移植

---
+++ iOS/Accountant/v1.0/Program/src/Accountant/Functions/API/WebAPIClientYouTube.swift
@@ -0,0 +1,112 @@
+//
+//  WebAPIClientYouTube.swift
+//  APIClientApp
+//
+//  Created by Hisashi Ishihara on 2023/01/19.
+//
+
+import Alamofire
+import Foundation
+
+struct EmptyBody: Decodable {}
+
+protocol RequestYouTube {
+    associatedtype Body
+    
+    var method: HTTPMethod { get }
+    var baseURL: String { get }
+    var path: String { get }
+    
+    var headers: HTTPHeaders? { get }
+    var parameters: [String: Any]? { get }
+    
+    func request<T: RequestYouTube>(_ req: req, completion: ((ResponseYouTube<req.Body>) -> Void)?)
+    func decode(from data: Data) throws -> ResponseYouTube<Body>
+}
+
+struct ResponseYouTube<Body> {
+    public let body: Body
+}
+
+extension RequestYouTube {
+    var method: HTTPMethod { .get }
+    var baseURL: String { "https://www.googleapis.com/" }
+    var path: String { "" }
+    
+    var headers: HTTPHeaders? { nil }
+    var parameters: [String: Any]? { nil }
+}
+
+struct DecodableResponseYouTube<Body: Decodable>: Decodable {
+    let body: Body
+    
+    private enum CodingKeys: CodingKey {
+        case body
+    }
+    
+    init(from decoder: Decoder) throws {
+        // キーがない階層の対応
+        body = try decoder.singleValueContainer().decode(Body.self)
+    }
+}
+
+extension RequestYouTube where Body: Decodable {
+    
+    func request<T: RequestYouTube>(_ req: req, completion: ((ResponseYouTube<req.Body>) -> Void)?) {
+        let req = AF.request(
+            req.baseURL + req.path,
+            method: req.method,
+            parameters: req.parameters,
+            encoding: URLEncoding.default,
+            // encoding: URLEncoding.queryString,
+            // encoding: JSONEncoding.default,
+            headers: req.headers
+        )
+            .response { response in
+                guard let data = response.data else { return }
+                
+                do {
+                    print(String(data: data, encoding: .utf8) ?? "変換できず")
+                    //                    {
+                    //                      "kind": "youtube#channelListResponse",
+                    //                      "etag": "RuuXzTIr0OoDqI4S0RU6n4FqKEM",
+                    //                      "pageInfo": {
+                    //                        "totalResults": 0,
+                    //                        "resultsPerPage": 5
+                    //                      }
+                    //                    }
+                    let model = try req.decode(from: data)
+                    completion?(model)
+                } catch let error {
+                    print("error decode json \(error)")
+                }
+            }
+        
+        debugPrint(req)
+    }
+    func decode(from data: Data) throws -> ResponseYouTube<Body> {
+        let response = try JSONDecoder().decode(DecodableResponseYouTube<Body>.self, from: data)
+        return ResponseYouTube<Body>(body: response.body)
+    }
+}
+
+// Use this for params
+extension Dictionary where Key == String, Value == Any? {
+    var cleaned: [Key: Any] { clean(self) }
+}
+
+func clean<Key, Value>(_ dict: [Key: Value?]) -> [Key: Value] {
+    var result: [Key: Value] = [:]
+    for (key, value) in dict {
+        if let value = value {
+            result[key] = value
+        }
+    }
+    return result
+}
+
+func sendApi<Req: RequestYouTube>(_ req: Req, completion: @escaping (ResponseYouTube<Req.Body>) -> Void) {
+    req.request(req) { response in
+        completion(response)
+    }
+}
---
+++ iOS/Accountant/v1.0/Program/src/Accountant/View/Settings/YouTube/MapTableViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  MapTableViewCell.swift
+//  APIClientApp
+//
+//  Created by Hisashi Ishihara on 2022/08/08.
+//
+
+import UIKit
+
+class MapTableViewCell: UITableViewCell {
+
+
+    @IBOutlet var thumbnailsImageView: UIImageView!
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var openingHoursLabel: UILabel!
+
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var starRatingStakView: UIView!
+    @IBOutlet var user_ratings_totalLabel: UILabel!
+    
+    @IBOutlet var type: UILabel!
+    @IBOutlet var publishedAtLabel: UILabel!
+
+
+    @IBOutlet var htmlTextView: UITextView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // 星を表示させる
+    func setRating(rating: Double) {
+        DispatchQueue.main.async {
+            let starRatingView = StarRatingView(frame: CGRect(origin: .zero, size: CGSize(width: self.starRatingStakView.bounds.width*0.9, height: self.starRatingStakView.bounds.height*0.9)),
+                                                rating: Float(rating),
+                                                color: UIColor.systemOrange,
+                                                starRounding: .floorToHalfStar)
+            self.starRatingStakView.addSubview(starRatingView)
+//            starRatingView.centerXAnchor.constraint(equalTo: self.starRatingStakView.centerXAnchor).isActive = true
+//            starRatingView.centerYAnchor.constraint(equalTo: self.starRatingStakView.centerYAnchor).isActive = true
+        }
+    }
+}
